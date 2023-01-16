cdc595c51bea39b341d4e9bbda3b4af7576df35e
MJ Kim <kim.mjds@gmail.com>
2023-01-16 15:13:43 +0900

    2번째 비밀정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const config = require('./config/key');
+const { User } = require('./models/User');
 
-const mongoose = require('mongoose')
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://mjkim:asdf1234@cluster0.8m6oj4u.mongodb.net/?retryWrites=true&w=majority', {
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB connect ...'))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World! Mongo DB Insert 예러 해결이 안되네요. 버젼')
+});
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    }) 
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
